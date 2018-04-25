48752be9dcc0290bb22bbd8971e65d23ce858083
alison.theabo@gmail.com <alison.theabo@gmail.com>
2018-04-25 22:44:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -14,11 +14,12 @@ function whileLoop(n) {
   return "done";
 }
 
-function maybeTrue() {
+
+function doWhileLoop(array) {
+  function maybeTrue() {
   return Math.random() >= 0.5
 }
 
-function doWhileLoop(array) {
   do {
     array.pop();
   } while (array.length > 0 && maybeTrue());
