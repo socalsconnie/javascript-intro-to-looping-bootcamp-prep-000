9ef93e78bc64e21476a1bb5a943ba71d23a67ddf
alison.theabo@gmail.com <alison.theabo@gmail.com>
2018-04-25 22:27:11 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-  for ( let i = 0; i < 25; i++) {
+  for ( let i = 0; i < 26; i++) {
     if (i === 1) {console.log("I am 1 strange loop.")
     } else {console.log(`I am ${i} strange loops.`)
     }
